77c2c6c5c297932fe6a4ac32ce1af8c94f40fae1
Kernel Dump <code@kerneldump.net>
2023-06-03 22:08:00 -0600

    Added likes.

--- tiktokfollowers.py
+++ tiktokfollowers.py
@@ -29,17 +29,20 @@ def procuser(userobj):
     useroutput = {}
     useroutput["name"] = userobj["name"]
     useroutput["followerCount"] = userobject["followerCount"]
+    useroutput["heartCount"] = userobject["heartCount"]
     useroutput["userid"] = userobj["userid"]
     try:
         with open(f'user-{userobj["userid"]}.json', "r") as userfile:
             lastoutput = json.load(userfile)
             useroutput["newFollowerCount"] = int(userobject["followerCount"]) - (lastoutput["followerCount"])
+            useroutput["newHeartCount"] = int(userobject["heartCount"]) - (lastoutput["heartCount"])
     except IOError:
         print(f'user-{userobj["userid"]}.json does not exist.')
     return useroutput
 
 def getuserstats():
     users = loadusers()
+    slackmsgdict = {}
     slackmsg = ":clock5: Hourly Follower Stats :clock6:\n"
     for user in users:
         print(user)
@@ -56,11 +59,30 @@ def getuserstats():
                 slackmsg += '  '
             slackmsg += f'{result["newFollowerCount"]}'
         slackmsg += "\n"
+    slackmsgdict["followers"] = slackmsg
 
-    return slackmsg
+    slackmsg = ":clock5: Hourly Like Stats :clock6:\n"
+    for user in users:
+        print(user)
+        result = procuser(user)
+        name = result["name"]
+        followers = result["heartCount"]
+        with open(f'user-{result["userid"]}.json', 'w') as f:
+            json.dump(result, f)
+        slackmsg += f'{result["name"]} {result["heartCount"]}'
+        if "newHeartCount" in result:
+            if int(result["newHeartCount"]) >= 0:
+                slackmsg += '  +'
+            else:
+                slackmsg += '  '
+            slackmsg += f'{result["newHeartCount"]}'
+        slackmsg += "\n"
+    slackmsgdict["likes"] = slackmsg
+    return slackmsgdict
 
 def main():
-    slackmsg = getuserstats()
+    slackmsgdict = getuserstats()
+    slackmsg = slackmsgdict["followers"]
     slack_key = os.getenv("slack_key")
     slack_webhook_stub = "https://hooks.slack.com/services/"
     slack_webhook_url = slack_webhook_stub + slack_key
@@ -69,6 +91,14 @@ def main():
         data='{"text": "' + slackmsg + '"}')
     print(slackmsg)
 
+    slackmsg = slackmsgdict["likes"]
+    slack_key = os.getenv("slack_key")
+    slack_webhook_stub = "https://hooks.slack.com/services/"
+    slack_webhook_url = slack_webhook_stub + slack_key
+    result = requests.post(
+        slack_webhook_url,
+        data='{"text": "' + slackmsg + '"}')
+    print(slackmsg)
 
 if __name__ == '__main__':
     main()
